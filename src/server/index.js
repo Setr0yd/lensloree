4926ee49a1cdc33cc2d1b31ef7b985451b2d5829
Setr0yd <aponamenko@inbox.ru>
2025-05-25 19:27:58 +0400

    Guss

--- src/server/index.js
+++ src/server/index.js
@@ -5,12 +5,9 @@ const app = express();
 // Правильные пути к роутам
 const productRoutes = require('./routes/products');
 const userRoutes = require('./routes/users');
-<<<<<<< HEAD
-=======
 const authRoutes = require('./routes/auth');
 const cartRoutes = require('./routes/cart');
 
->>>>>>> 73c3102 (Guss)
 
 // Middleware
 app.use(express.json());
@@ -19,11 +16,8 @@ app.use(express.static(path.join(__dirname, '../client')));
 // Роуты
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-<<<<<<< HEAD
-=======
 app.use('/api/auth', authRoutes);
 app.use('/api/cart', cartRoutes);
->>>>>>> 73c3102 (Guss)
 
 // Главная страница
 app.get('/', (req, res) => {
@@ -41,11 +35,6 @@ app.listen(PORT, () => {
   🚀 Сервер запущен: http://localhost:${PORT}
   📦 API: http://localhost:${PORT}/api/products
   👤 Users: http://localhost:${PORT}/api/users
-<<<<<<< HEAD
-  `);
-});
-=======
   🔐 Auth: http://localhost:${PORT}/api/auth
   ` );
 });
->>>>>>> 73c3102 (Guss)
